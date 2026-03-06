f9e076b0c910ca4d39fc097cc3b8e2d64c88d9d3
PatilNinad18 <iampatil1804@gmail.com>
2026-03-06 16:35:32 +0530

    Fix cors production errors

--- personal-application-tracker/backend/src/app.js
+++ personal-application-tracker/backend/src/app.js
@@ -11,10 +11,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 // routes FIRST
